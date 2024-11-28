f4572728726086c28752372d1b676df380fe204a
opechapo <abolajiopeyemi000@gmail.com>
2024-11-28 04:55:13 +0100

    htmlandcss

---
+++ calc/src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+
+function reducer(type, payload) {
+  
+}
+
+function App() {
+const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='precious-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
+
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>%</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  )
+}
+
+export default App
